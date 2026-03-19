659466ee48b6313f6abec251a45299b896527726
Riley-Lin123 <rtlin94122@gmail.com>
2026-03-18 17:32:57 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-// class Main {
-//   public static void main(String[] args) {
-//     Sketch main_tester = new Sketch();
-//     main_tester.tester();
-//   }
-// }
+class Main {
+  public static void main(String[] args) {
+    Sketch main_tester = new Sketch();
+    main_tester.tester();
+  }
+}
 
 
 
